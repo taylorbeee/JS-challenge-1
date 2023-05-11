0ccaebacd7d1ab9fe4539f1178d5143ed0a35c38
Taylor Behrens <126136711+taylorbeee@users.noreply.github.com>
2023-05-11 14:34:45 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// console.log("Hi Taylor!");
+var mainTitle = document.querySelector("h1");
+mainTitle.innerText = "Yay, Javascript!!";
+
+console.log(mainTitle);
+
+var body = document.querySelector("body");
+body.style.backgroundColor = "#FEF2F1";
+console.log(body);
